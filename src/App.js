b6bfe87f5ab71fc9f987cf57af05bc778a8c5907
Kayla Ward <kaylaw0127@gmail.com>
2020-05-20 20:16:28 -0700

    linked component folders to app js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import About from "./components/about/about";
+import Contact from "./components/contact/contact";
+import Navbar from "./components/navbar/navbar";
+import Portfolio from "./components/portfolio/portfolio";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+  
+          <About />
+          <Contact />
+          <Navbar />
+          <Portfolio />
+        
+      </div>
+      
+    )
+}
 }
 
 export default App;
