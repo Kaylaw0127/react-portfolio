b867451fdb3007b42d9f99d80aad839a5e54e92b
Kayla Ward <kaylaw0127@gmail.com>
2020-05-22 21:34:27 -0700

    added react mdl

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,9 @@
 import React from "react"
+import './style.css'
 
 function Navbar()  {
     return (
-        <div className="navigator">
+        <div id="navbar" className="nav">
             <nav class="navbar navbar-expand-lg navbar-light bg-light pt-0 pb-0">
                 <a class="navbar-brand text-white font-weight-bold p-3 ml-5" href="#">Kayla Ward</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
@@ -12,21 +13,20 @@ function Navbar()  {
                 <a className="navbar-brand mx-auto" href="#" target="_blank">Resume</a>
                 <a className="navbar-brand mx-auto" href="https://www.linkedin.com/in/kaylaw0127/">LinkedIn</a>
                 <a className="navbar-brand mx-auto" href="https://github.com/Kaylaw0127">Github</a>
-            <div className="collapse navbar-collapse" id="navbarNav">
-
-            <ul className="navbar-nav">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#portfolio">Portfolio</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#contact">Contact</a>
-                </li>
-
-                    </ul>
-                </div>
+           
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="portfolio.html">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
             </nav>
     </div> 
     )
