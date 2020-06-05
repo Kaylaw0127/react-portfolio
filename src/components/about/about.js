380e9433956abdb4ff32bf44bbd9073914e3f040
Kayla Ward <kaylaw0127@gmail.com>
2020-06-04 18:35:58 -0700

    updated experience portion in about

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -15,7 +15,7 @@ class About extends Component {
                 <div className="grid">
               <img src= {picture} alt="ProfilePic" />
               
-                <p class="paragraph"> I am a full stack web developer with a background in business management and human resources. I am highly knowledgable with both client and server side languages, responsive frameworks, and best code practices. I am experienced with HTML, CSS, JavaScript, jQuery, API's, and Node.js. As a graduate from the UC Davis Coding Bootcamp,  I have developed numerous skills, as well as strengthened existing abilities. I am dedicated to perfecting my craft while working along side seasoned developers.
+                <p class="paragraph"> I am a full stack web developer with a background in business management and human resources. I am highly knowledgable with both client and server side languages, responsive frameworks, and best code practices. I am experienced with HTML, CSS, JavaScript, jQuery, API's, Node.js, React.js, MongoDB, and MySQL. As a graduate from the UC Davis Coding Bootcamp,  I have developed numerous skills, as well as strengthened existing abilities. I am dedicated to perfecting my craft while working along side seasoned developers.
                 </p>
             </div>
             </Cell>
